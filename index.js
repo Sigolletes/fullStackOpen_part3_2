f18f57c957070e8065eb0cf74b66e4c9671df093
Sigolletes <webdevjudit@gmail.com>
2023-02-23 13:34:26 +0100

    Add lint configuration

--- index.js
+++ index.js
@@ -1,105 +1,111 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable consistent-return */
 // DOTENV
-require('dotenv').config()
+require('dotenv').config();
+
+// MIDDLEWARE MORGAN CALL
+const morgan = require('morgan');
+
+// MIDDLEWARE CORS CALL
+const cors = require('cors');
+
+// MIDDLEWARE EXPRESS CALL
+const express = require('express');
 
 // IMPORT MODULE person.js
-const Person = require('./models/person')
+const Person = require('./models/person');
 
 // EXPRESS FRAMEWORK FOR NODE.JS
-const express = require('express')
-const app = express()
+const app = express();
 
 // MIDDLEWARE CORS (Cross-Origin Resource Sharing) FOR ALLOW CROSS-ORIGIN REQUESTS
-const cors = require('cors')
-app.use(cors())
+app.use(cors());
 
-// MIDDLEWARE STATIC FROM EXPRESS FOR SHOW INDEX.HTML FROM BUILD FOLDER IF THE GET REQUEST ADDRESS CORRESPOND TO www.example.com/index.html OR www.example.com
-app.use(express.static('build'))
+// MIDDLEWARE STATIC FROM EXPRESS FOR SHOW INDEX.HTML FROM BUILD
+// FOLDER IF THE GET REQUEST ADDRESS CORRESPOND TO
+// www.example.com/index.html OR www.example.com
+app.use(express.static('build'));
 
 // EXPRESS JSON-PARSER FOR ACCESS DATA EASILY (creates body property)
-app.use(express.json())
+app.use(express.json());
 
 // MIDDLEWARE MORGAN FOR LOGGING MESSAGES TO THE CONSOLE
-const morgan = require('morgan')
-
-morgan.token("info", (req, res) => {
-  const { body } = req
-  return JSON.stringify(body)
-})
+morgan.token('info', (req, res) => {
+  const { body } = req;
+  return JSON.stringify(body);
+});
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :info")
-)
+  morgan(':method :url :status :res[content-length] - :response-time ms :info'),
+);
 
-morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms'
-  ].join(' ')
-})
+morgan((tokens, req, res) => [
+  tokens.method(req, res),
+  tokens.url(req, res),
+  tokens.status(req, res),
+  tokens.res(req, res, 'content-length'), '-',
+  tokens['response-time'](req, res), 'ms',
+].join(' '));
 
-// PEOPLE ARRAY
-let people = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+// // PEOPLE ARRAY
+// let people = [
+//   {
+//     'id': 1,
+//     'name': 'Arto Hellas',
+//     'number': '040-123456'
+//   },
+//   {
+//     'id': 2,
+//     'name': 'Ada Lovelace',
+//     'number': '39-44-5323523'
+//   },
+//   {
+//     'id': 3,
+//     'name': 'Dan Abramov',
+//     'number': '12-43-234345'
+//   },
+//   {
+//     'id': 4,
+//     'name': 'Mary Poppendieck',
+//     'number': '39-23-6423122'
+//   }
+// ];
 
 // RESPONSES TO THE REQUESTS (ROUTES)
-
 app.get('/', (request, response) => {
-  response.send('<h1>PHONEBOOK</h1><h2>FULL STACK OPEN PART 3</h2><h3>Exercise by <a href="https://github.com/Sigolletes" target="blank"><em>Sigolletes</em></a></h3>')
-})
+  response.send('<h1>PHONEBOOK</h1><h2>FULL STACK OPEN PART 3</h2><h3>Exercise by <a href="https://github.com/Sigolletes" target="blank"><em>Sigolletes</em></a></h3>');
+});
 
 app.get('/info', (request, response) => {
-  let date = new Date
+  const date = new Date();
 
-  Person.count({}, function(error, result, next) {
+  Person.count({}, (error, result, next) => {
     if (error) {
-      next(error)
+      next(error);
     } else {
-      response.send(`<h3>This Phonebook has information for ${result} people</h3><h3>${date}</h3>`)
+      response.send(`<h3>This Phonebook has information for ${result} people</h3><h3>${date}</h3>`);
     }
-  })
-})
+  });
+});
 
 app.get('/api/people', (request, response, next) => {
-  Person.find({}).then(people => {
-    response.json(people)
+  Person.find({}).then((people) => {
+    response.json(people);
   })
-  .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 app.get('/api/people/:id', (request, response, next) => {
   Person.findById(request.params.id)
-  .then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
-})
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
 // const generateId = () => {
 //   let num
@@ -110,37 +116,37 @@ app.get('/api/people/:id', (request, response, next) => {
 // }
 
 app.post('/api/people', (request, response, next) => {
-  const body = request.body
+  const { body } = request;
 
   if (!body.name) {
-    return response.status(400).json({ 
-      error: 'Name missing' 
-    })
+    return response.status(400).json({
+      error: 'Name missing',
+    });
   }
   if (!body.number) {
-    return response.status(400).json({ 
-      error: 'Number missing' 
-    })
+    return response.status(400).json({
+      error: 'Number missing',
+    });
   }
   // if (people.some(p => p.name === body.name)) {
-  //   return response.status(400).json({ 
-  //     error: 'This name already exists in the phonebook' 
+  //   return response.status(400).json({
+  //     error: 'This name already exists in the phonebook'
   //   })
   // }
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  })
+  });
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
   })
-  .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 app.put('/api/people/:id', (request, response, next) => {
-  const { name, number } = request.body
+  const { name, number } = request.body;
 
   // const person = {
   //   name: body.name,
@@ -148,46 +154,47 @@ app.put('/api/people/:id', (request, response, next) => {
   // }
 
   Person.findByIdAndUpdate(
-    request.params.id, 
+    request.params.id,
     { name, number },
-    { new: true, runValidators: true, context: 'query' }
+    { new: true, runValidators: true, context: 'query' },
   )
-    .then(updatedPerson => {
-      response.json(updatedPerson)
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 app.delete('/api/people/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
+    .then((result) => {
+      response.status(204).end();
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 // MIDDLEWARE FOR CATCHING REQUESTS MADE TO NON-EXISTENT ROUTES (it has to be after routes)
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'Unknown Endpoint' })
-}
-app.use(unknownEndpoint)
+  response.status(404).send({ error: 'Unknown Endpoint' });
+};
+app.use(unknownEndpoint);
 
 // ERROR HANDLER MIDDLEWARE
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+    return response.status(400).send({ error: 'malformatted id' });
+  } if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
-  next(error)
-}
+  next(error);
+};
 
 // this has to be the last loaded middleware.
-app.use(errorHandler)
+app.use(errorHandler);
 
-// THE WEB SERVER CREATED WITH EXPRESS (APP) IS ASSIGNED TO A PORT AND RESPOND TO THE REQUESTS OF THAT PORT
-const PORT = process.env.PORT
+// THE WEB SERVER CREATED WITH EXPRESS (APP) IS ASSIGNED TO A PORT AND RESPOND
+// TO THE REQUESTS OF THAT PORT
+const { PORT } = process.env;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
--- models/person.js
+++ models/person.js
@@ -1,45 +1,49 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
 // MODULE FOR MONGOOSE
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log('connecting to', url);
 
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
+  .then((result) => {
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
-    required: true
+    required: true,
   },
   number: {
     type: String,
     validate: {
-      validator: function(num) {
-        return /^\d{8,}|^\d{2}[-]\d{6,}|^\d{3}[-]\d{5,}/.test(num)
+      validator(num) {
+        return /^\d{8,}|^\d{2}[-]\d{6,}|^\d{3}[-]\d{5,}/.test(num);
       },
-      message: props => `${props.value} is not a valid phone number`
+      message: (props) => `${props.value} is not a valid phone number`,
     },
-    required: [true, 'User phone number required']
+    required: [true, 'User phone number required'],
   },
-})
+});
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+    delete returnedObject._v;
+  },
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
